a024eb32ce02085106e9abafe3370ad8ab12eb1d
Sharif Marmol <marmol.sharif@gmail.com>
2022-07-05 19:55:53 -0300

    loop

---
+++ main.js
@@ -0,0 +1,13 @@
+// FOR (desde;hasta;actualizacion) 
+
+for(let i = 0; i < 3; i++) {
+    let nombre = prompt("Cual es tu nombre ?");
+
+    let deporte = prompt("Cual es tu deporte favorito ?");
+
+    let animal = prompt("Cual es el animal con el cual mas te identificas ?");
+
+    console.log(`Mi nombre es ${nombre}, mi deporte favorito es el ${deporte}, y me identifico con el ${animal}.`);
+
+}
+
